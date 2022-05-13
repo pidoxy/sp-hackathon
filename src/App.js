3c090154a3c3c3eb29b8aa69b543b4321617bab3
IDOKO EMMANUEL <pidox31102002@gmail.com>
2022-05-14 00:27:04 +0100

    setup header, sidebar, protectedrouted, error boundary, theme and dashboard page

--- src/App.js
+++ src/App.js
@@ -2,26 +2,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/signup";
 import Login from "./pages/login";
 
-import ProtectedRoutes from './components/ProtectedRoutes'
+import ProtectedRoutes from "./components/ProtectedRoutes";
 import Dashboard from "./pages/dashboard";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeContext, ThemeContextProvider } from "./context/ThemeContext";
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          {/** Protected Routes */}
-        {/** Wrap all Route under ProtectedRoutes element */}
-        <Route path="/" element={<ProtectedRoutes/>}>
-          <Route path="dashboard" element={<Dashboard/>} />
-        </Route>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      <ErrorBoundary>
+        <ThemeContextProvider>
+          <BrowserRouter>
+            <Routes>
+              {/** Protected Routes */}
+              {/** Wrap all Route under ProtectedRoutes element */}
+              {/* <Route path="/" element={<ProtectedRoutes/>}> */}
+              <Route path="dashboard" element={<Dashboard />} />
+              {/* </Route> */}
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeContextProvider>
+      </ErrorBoundary>
     </>
   );
-}
+};
 
 export default App;
